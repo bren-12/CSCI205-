2e50e29671605d64c5dc7b04d63e954b9594a7ea
unknown <brennanedmonds@gmail.com>
2020-08-05 09:03:25 -0400

    Ch1.5 Practice

--- hw.cpp
+++ hw.cpp
@@ -6,11 +6,19 @@ using namespace std;
 
 int main()
 {
-    vector<string> msg {"Hello3", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+    /*
+    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
     
     for (const string& word : msg)
     {
         cout << word << " ";
     }
     cout << endl;
+    */
+   int number {2};
+   std::cout << "Enter a number and it will be printed below" << '\n' << '\n'; 
+   std::cin >> number;
+   std::cout << number;
+
+   return 0;
 }
